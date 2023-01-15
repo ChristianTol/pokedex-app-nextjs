8c9b3e90ef7ae5f9271a1c0d9c4816181dbdd822
ChristianTol <christian.tol1998@hotmail.com>
2023-01-15 13:45:50 +0100

    Loading animation changed

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -5,7 +5,7 @@ import pokeball from "../public/pokeball-2.png";
 export const Loader = () => {
   return (
     <div className="flex justify-center items-center">
-      <figure className="animate-[spin_5s_linear_infinite]">
+      <figure className="animate-[spin_4s_infinite]">
         <Image src={pokeball} alt="Loading..." height="100px" width="100px" />
       </figure>
     </div>
