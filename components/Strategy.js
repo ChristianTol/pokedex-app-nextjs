5ca4d683e3a48783c3316f0b5ecb173ce36c7dbb
ChristianTol <christian.tol1998@hotmail.com>
2024-08-17 20:23:15 +0200

    strategy divs and types styling updated.

--- components/Strategy.js
+++ components/Strategy.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { capitalizeFirstLetter } from '../helper/helper';
 import Image from 'next/image';
+import { formatPokemonName } from './Api';
 
 const Strategy = ({ pokemonDetails }) => {
   const [typeRelations, setTypeRelations] = useState(null);
@@ -84,109 +85,119 @@ const Strategy = ({ pokemonDetails }) => {
     fetchTypeRelations();
   }, [pokemonDetails]);
 
-  
-    const renderTypeList = (types, multiplier) => {
-        if (types.size === 0) return null;
-        
-        return (
-        <div className="mb-6">
-            <ul className="flex flex-wrap gap-2">
-            {Array.from(types).map(type => (
-                <span
-                key={type}
-                className={`px-2 py-0 md:py-3 rounded flex items-center justify-center sm:px-3 sm:py-1 sm:gap-1
-                ${isMobile && type}`}
+  const PokemonDisplay = ({ pokemonDetails }) => (
+    <div className="mb-6 text-white rounded-lg p-4 shadow-md w-[300px] md:w-[400px] mx-auto" style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
+      <h2 className="text-2xl font-bold mb-2">{capitalizeFirstLetter(pokemonDetails.name)}</h2>
+      <img 
+        src={pokemonDetails.sprites.other['official-artwork'].front_default} 
+        alt={pokemonDetails.name}
+        className="mx-auto w-40 h-40"
+      />
+      <div className="flex gap-4 my-2 justify-center">
+        {pokemonDetails.types?.map((type) => (
+          <span
+            key={type.type.name}
+            className={`px-2 py-0 md:py-3 rounded flex items-center justify-center 
+            ${isMobile && type.type.name}`}
+            onClick={(e) => {
+              const newType = type.type.name;
+              const currentType = filters.type;
+              const updatedType = newType === currentType ? "all" : newType;
+              updateFilters({ type: updatedType });
+            }}
+          >
+            <div className="md:hidden">
+              <Image
+                src={`/icons/${type.type.name}.svg`}
+                alt={`${type.type.name}`}
+                width={15}
+                height={15}
+              />
+            </div>
+            <div className="hidden md:flex">
+              <div
+                className={`${type.type.name} rounded-l m-[-8px] [clip-path:polygon(0%_0%,100%_0%,80%_100%,0%_100%)]`}
               >
-                <div className="md:hidden">
+                <div className="h-[32px] w-[45px] flex items-center justify-center pr-[10px]">
                   <Image
-                    src={`/icons/${type}.svg`}
-                    alt={`${type}`}
-                    width={15}
-                    height={15}
+                    className=""
+                    src={`/icons/${type.type.name}.svg`}
+                    alt={`${type.type.name}`}
+                    width={20}
+                    height={20}
                   />
                 </div>
-                <div className="hidden md:flex">
-                  <div
-                    className={`${type} rounded-l m-[-8px] [clip-path:polygon(0%_0%,100%_0%,80%_100%,0%_100%)]`}
-                  >
-                    <div className="h-[32px] w-[45px] flex items-center justify-center pr-[10px]">
-                      <Image
-                        className=""
-                        src={`/icons/${type}.svg`}
-                        alt={`${type}`}
-                        width={20}
-                        height={20}
-                      />
-                    </div>
-                  </div>
-                  <div
-                    className={`h-[32px] w-[90px] flex items-center justify-center m-[-8px] rounded-r ${type}-text font-bold [clip-path:polygon(10%_0%,100%_0%,100%_100%,0%_100%)]`}
-                  >
-                    <h6 className="hidden md:inline uppercase px-[8px]">
-                        {capitalizeFirstLetter(type)}
-                    </h6>
-                  </div>
-                </div>
-              </span>
-            ))}
-            </ul>
-        </div>
-        );
-    };
+              </div>
+              <div
+                className={`h-[32px] w-[90px] flex items-center justify-center m-[-8px] rounded-r ${type.type.name}-text font-bold [clip-path:polygon(10%_0%,100%_0%,100%_100%,0%_100%)]`}
+              >
+                <h6 className="hidden md:inline uppercase px-[8px]">
+                  {formatPokemonName(type.type.name)}
+                </h6>
+              </div>
+            </div>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 
-    if (!typeRelations) {
-        return <div>Loading type relations...</div>;
-    }
-
-    const renderSection = (title, items) => {
-        const nonEmptySections = items.filter(item => item.types.size > 0);
-        if (nonEmptySections.length === 0) return null;
+  
+  const renderTypeList = (types, label) => {
+    if (types.size === 0) return null;
     
-        return (
-            <div className="mb-6 text-white rounded-lg p-4 shadow-md" style={{ background: 'rgba(0, 0, 0, 0.3)'}}>
-                {items.map(item => (
-                    <div key={item.multiplier}>
-                        <h3 className="text-xl font-semibold mb-3" style={{fontSize: '20px'}}>
-                            {title} ({item.multiplier}x)
-                        </h3>
-                        {renderTypeList(item.types, item.multiplier)}
-                    </div>
-                ))}
-            </div>
-        );
-    };
-
     return (
-        <div className="strategy-container">
-            <h2 className="text-2xl font-bold mb-6 text-center">Type Effectiveness</h2>
-            
-            <div className="flex flex-col md:flex-row justify-between">
-            <div className="w-full md:w-1/2 pr-0 md:pr-2" style={{maxWidth: '500px'}}>
-                {renderSection("Super Effective", [
-                { types: typeRelations.double_damage_to, multiplier: 2 }
-                ])}
-                {renderSection("Not Very Effective", [
-                { types: typeRelations.half_damage_to, multiplier: 0.5 },
-                ])}
-                {renderSection("No Effect", [
-                { types: typeRelations.no_damage_to, multiplier: 0 }
-                ])}
-            </div>
-
-            <div className="w-full md:w-1/2 pl-0 md:pl-2 mt-4 md:mt-0" style={{maxWidth: '500px'}}>
-                {renderSection("Very weak", [
-                { types: typeRelations.double_damage_from, multiplier: 2 }
-                ])}
-                {renderSection("Resistance", [
-                { types: typeRelations.half_damage_from, multiplier: 0.5 },
-                ])}
-                {renderSection("Immunity", [
-                { types: typeRelations.no_damage_from, multiplier: 0 }
-                ])}
-            </div>
-            </div>
+      <div className="mb-4 ">
+        <h4 className="font-semibold mb-2 text-lg">{label}</h4>
+        <div className="flex flex-wrap gap-2">
+          {Array.from(types).map(type => (
+            <span 
+              key={type} 
+              className={`type-icon ${type} rounded-full p-2 flex items-center justify-center`}
+              style={{ 
+                width: '48px', 
+                height: '48px', 
+                backgroundColor: `${type}`
+              }}
+            >
+              <Image
+                src={`/icons/${type}.svg`}
+                alt={type}
+                width={32}
+                height={32}
+                className="rounded-full"
+              />
+            </span>
+          ))}
         </div>
+      </div>
     );
+  };
+
+  if (!typeRelations) {
+    return <div>Loading type relations...</div>;
+  }
+
+  return (
+    <div className="strategy-container ">
+      <PokemonDisplay pokemonDetails={pokemonDetails} />
+      
+      <div className="type-effectiveness grid grid-cols-1 md:grid-cols-2 gap-4 ">
+      <div className="mb-6 text-white rounded-lg p-4 shadow-md w-[300px] md:w-[500px] mx-auto" style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
+          <h3 className="text-xl font-bold mb-2">Offensive</h3>
+          {renderTypeList(typeRelations.double_damage_to, "Super Effective")}
+          {renderTypeList(typeRelations.half_damage_to, "Not very effective")}
+          {renderTypeList(typeRelations.no_damage_to, "No effect")}
+        </div>
+        <div className="mb-6 text-white rounded-lg p-4 shadow-md w-[300px] md:w-[500px] mx-auto" style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
+          <h3 className="text-xl font-bold mb-2">Defensive</h3>
+          {renderTypeList(typeRelations.double_damage_from, "Vulnerable")}
+          {renderTypeList(typeRelations.half_damage_from, "Resistant")}
+          {renderTypeList(typeRelations.no_damage_from, "Immune")}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Strategy;
